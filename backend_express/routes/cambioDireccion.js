8d11b6ad64d7563496ed565bef98c4e425de1796
PaulRojasUN <82885703+PaulRojasUN@users.noreply.github.com>
2022-08-12 13:50:23 -0500

    Se puede cambiar la direccion y datos del medio de pago

--- backend_express/routes/cambioDireccion.js
+++ backend_express/routes/cambioDireccion.js
@@ -11,4 +11,32 @@ router.get('/', function (req, res, next)
     res.render('cambioDireccion', { title: 'Express' });
 });
 
+router.post('/', function (req, res, next) {
+    connect(function (err, client, done) {
+      if (err) {
+        return console.error('error fetching client from pool', err);
+      }
+      console.log(req.body);
+      console.log("Cambio dirección");
+      if (req.body.boton == "cambio")
+      {
+
+            client.query(`UPDATE Cliente SET direccion = '${req.body.direccion}' WHERE celular = '${req.body.celular}'`, function (err, result) {
+            //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+            done(err);
+            if (err) {
+              return console.error('error running query', err);
+            }
+            console.log("Se cambió la direccion");
+            res.render('mainCliente', {celular: req.body.celular});
+          });
+      }
+      else
+      {
+        res.render('ajustes', {celular: req.body.celular});
+      }
+    });
+  
+  })
+
 module.exports = router;
--- backend_express/routes/cambioMedioPago.js
+++ backend_express/routes/cambioMedioPago.js
@@ -11,4 +11,33 @@ router.get('/', function (req, res, next)
     res.render('cambioMedioPago', { title: 'Express' });
 });
 
+router.post('/', function (req, res, next) {
+    connect(function (err, client, done) {
+      if (err) {
+        return console.error('error fetching client from pool', err);
+      }
+      console.log(req.body);
+      console.log("Cambio Medio Pago");
+      if (req.body.boton == "cambio")
+      {
+            client.query(`UPDATE Tarjeta SET tipo = '${req.body.radioB}' WHERE celular = '${req.body.celular}';
+            UPDATE Tarjeta SET noTarjeta = '${req.body.numero}' WHERE celular = '${req.body.celular}';
+            UPDATE Tarjeta SET codigoSeguridad = '${req.body.codigo}' WHERE celular = '${req.body.celular}';`, function (err, result) {
+            //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+            done(err);
+            if (err) {
+              return console.error('error running query', err);
+            }
+            console.log("Se cambió el medio pago");
+            res.render('mainCliente', {celular: req.body.celular});
+          });
+      }
+      else
+      {
+        res.render('ajustes', {celular: req.body.celular});
+      }
+    });
+  
+  })
+
 module.exports = router;
